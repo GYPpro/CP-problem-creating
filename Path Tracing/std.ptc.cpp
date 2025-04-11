5c9798f9bc71dc85ce0c8494bba26b6acb5008ed
Guo Yanpei <73223812+GYPpro@users.noreply.github.com>
2025-04-11 13:51:34 +0800

    sync

--- Path Tracing/std.ptc.cpp
+++ Path Tracing/std.ptc.cpp
@@ -33,6 +33,8 @@ struct Frac {
         }
     }
     void reduce() {
+        assert(abs(num) <= 1e18);
+        assert(abs(den) <= 1e18);
         T g = std::gcd(num, den);
         num /= g;
         den /= g;
@@ -113,10 +115,238 @@ struct Frac {
     }
 };
  
-using F = Frac<int>;
+using F = Frac<__int128>;
 
-using dot = pair<F,F>;
-using lin = pair<dot,dot>;
+F max(F a,F b) {
+    return a < b ? b : a;
+}
+
+F min(F a,F b) {
+    return a < b ? a : b;
+}
+
+F abs(F a) {
+    return a < F(0) ? -a : a;
+}
+
+namespace std {
+template<typename T>
+class Interval {
+public:
+    // 构造函数，默认为闭区间
+    Interval(T a, T b, bool a_closed = true, bool b_closed = true)
+        : l(a), r(b), lc(a_closed), rc(b_closed) {}
+
+    // 判断区间是否非空（当 l < r 或 l==r 且两端都闭合时有效）
+    bool valid() const {
+        return (l < r) || (l == r && lc && rc);
+    }
+
+    // 判断两个区间是否有交集（考虑“接触”情况：若接触端点至少一侧闭合，则认为交集存在）
+    bool overlaps(const Interval &o) const {
+        if(r < o.l || (r == o.l && (!rc || !o.lc))) return false;
+        if(o.r < l || (o.r == l && (!o.rc || !lc))) return false;
+        return true;
+    }
+
+    // 交集运算：返回两个区间的交集（为空时返回一个无效区间）
+    Interval intersect(const Interval &o) const {
+        T L = max(l, o.l);
+        T R = min(r, o.r);
+        bool Lc = (l == o.l ? (lc && o.lc) : (l < o.l ? o.lc : lc));
+        bool Rc = (r == o.r ? (rc && o.rc) : (r < o.r ? rc : o.rc));
+        Interval ret(L, R, Lc, Rc);
+        // 交集无效，则返回一个无效区间（这里用闭合全 false 表示无效）
+        return ret.valid() ? ret : Interval(L, L, false, false);
+    }
+
+    // 并集运算：若两个区间重叠或相邻（接触时至少一侧闭合），返回合并后的单一区间，
+    // 否则返回两个区间
+    vector<Interval> unite(const Interval &o) const {
+        // 判断“相邻”，如 a.r == b.l 且至少一侧闭合
+        bool adjacent = (r == o.l && (rc || o.lc)) || (o.r == l && (o.rc || lc));
+        if(!overlaps(o) && !adjacent) return {*this, o};
+        T L = (l < o.l ? l : o.l);
+        T R = (r > o.r ? r : o.r);
+        bool Lc = (l < o.l ? lc : (l > o.l ? o.lc : (lc || o.lc)));
+        bool Rc = (r > o.r ? rc : (r < o.r ? o.rc : (rc || o.rc)));
+        return { Interval(L, R, Lc, Rc) };
+    }
+
+    // 差集运算：计算当前区间减去 o 后剩余的部分，最多返回两个区间
+    vector<Interval> subtract(const Interval &o) const {
+        // 若没有重叠，则整个区间保留
+        if(!overlaps(o)) return {*this};
+        Interval inter = intersect(o);
+        vector<Interval> res;
+        // 左侧剩余区间存在条件：当前区间左端小于交集左端，
+        // 或相等但当前左端闭合而交集左端不闭合
+        if(l < inter.l || (l == inter.l && lc && !inter.lc))
+            res.push_back(Interval(l, inter.l, lc, false));
+        // 右侧剩余区间
+        if(r > inter.r || (r == inter.r && rc && !inter.rc))
+            res.push_back(Interval(inter.r, r, false, rc));
+        return res;
+    }
+
+    // 方便输出，格式如 [l,r) 表示区间的开闭情况
+    void print() const {
+        cout << (lc ? "[" : "(") << l << "," << r << (rc ? "]" : ")");
+    }
+
+private:
+    T l, r;       // 区间左右端点
+    bool lc, rc;  // 分别表示左、右端点是否闭合（true：闭区间，false：开区间）
+};
+
+template <typename TYPE_NAME,std::size_t N>
+class Vector {
+public:
+    std::array<TYPE_NAME, N> data;
+
+    TYPE_NAME & first = data[0];
+    TYPE_NAME & second = data[1];
+
+    // 默认构造函数：初始化为 0
+    Vector() {
+        data.fill(0.0);
+    }
+
+    Vector(TYPE_NAME first,TYPE_NAME second) {
+        data[0] = first;
+        data[1] = second;
+    }
+
+    // 使用 initializer_list 构造
+    Vector(std::initializer_list<TYPE_NAME> list) {
+        assert(list.size() == N && "Initializer list size must equal vector dimension.");
+        std::copy(list.begin(), list.end(), data.begin());
+    }
+
+    //拷贝构造
+    Vector(const Vector &other) {
+        std::copy(other.data.begin(), other.data.end(), data.begin());
+    }
+
+    // 移动构造函数
+    Vector(Vector &&other) noexcept {
+        std::move(other.data.begin(), other.data.end(), data.begin());
+    }
+    // 移动赋值运算符
+    Vector& operator=(Vector &&other) noexcept {
+        if (this != &other) {
+            std::move(other.data.begin(), other.data.end(), data.begin());
+        }
+        return *this;
+    }
+
+    // 下标运算符
+    TYPE_NAME& operator[](std::size_t index) {
+        assert(index < N);
+        return data[index];
+    }
+    const TYPE_NAME& operator[](std::size_t index) const {
+        assert(index < N);
+        return data[index];
+    }
+
+
+
+    // 向量加法
+    Vector operator+(const Vector &other) const {
+        Vector result;
+        for (std::size_t i = 0; i < N; ++i)
+            result.data[i] = data[i] + other.data[i];
+        return result;
+    }
+
+    // 向量减法
+    Vector operator-(const Vector &other) const {
+        Vector result;
+        for (std::size_t i = 0; i < N; ++i)
+            result.data[i] = data[i] - other.data[i];
+        return result;
+    }
+
+    // 数乘：向量 * 标量（右侧为标量）
+    Vector operator*(TYPE_NAME scalar) const {
+        Vector result;
+        for (std::size_t i = 0; i < N; ++i)
+            result.data[i] = data[i] * scalar;
+        return result;
+    }
+
+    // 相等
+    bool operator==(const Vector &other) const {
+        for (std::size_t i = 0; i < N; ++i) {
+            if (data[i] != other.data[i]) return false;
+        }
+        return true;
+    }
+
+    // 数乘：标量 * 向量（友元函数）
+    friend Vector operator*(TYPE_NAME scalar, const Vector &vec) {
+        return vec * scalar;
+    }
+
+    // 点乘（内积）
+    TYPE_NAME dot(const Vector &other) const {
+        TYPE_NAME result = 0.0;
+        for (std::size_t i = 0; i < N; ++i)
+            result += data[i] * other.data[i];
+        return result;
+    }
+
+    // 求向量的欧几里得范数
+    TYPE_NAME norm() const {
+        return std::sqrt(this->dot(*this));
+    }
+
+    // 输出运算符重载
+    friend std::ostream& operator<<(std::ostream &os, const Vector &vec) {
+        os << "[";
+        for (std::size_t i = 0; i < N; ++i) {
+            os << vec.data[i];
+            if (i != N - 1)
+                os << ", ";
+        }
+        os << "]";
+        return os;
+    }
+};
+
+template <std::size_t I, typename TYPE_NAME,std::size_t N>
+constexpr TYPE_NAME& get(Vector<TYPE_NAME,N>& vec) noexcept {
+    static_assert(I < N, "Index out of bounds in Vector::get");
+    return vec.data[I];
+}
+
+template <std::size_t I,typename TYPE_NAME, std::size_t N>
+constexpr const TYPE_NAME& get(const Vector<TYPE_NAME,N>& vec) noexcept {
+    static_assert(I < N, "Index out of bounds in Vector::get (const)");
+    return vec.data[I];
+}
+
+template <std::size_t I,typename TYPE_NAME, std::size_t N>
+constexpr TYPE_NAME&& get(Vector<TYPE_NAME,N>&& vec) noexcept {
+    static_assert(I < N, "Index out of bounds in Vector::get (rvalue)");
+    return std::move(vec.data[I]);
+}
+
+    template <typename TYPE_NAME,std::size_t N>
+    struct tuple_size<Vector<TYPE_NAME,N>> : std::integral_constant<std::size_t, N> { };
+
+    template <typename TYPE_NAME,std::size_t I, std::size_t N>
+    struct tuple_element<I, Vector<TYPE_NAME,N>> {
+        using type = TYPE_NAME;
+    };
+}
+
+// using dot = pair<F,F>;
+// using lin = pair<dot,dot>;
+
+using dot = Vector<F, 2>;
+using lin = pair<dot, dot>;
 
 // using convexHull = vector<dot>;
 
@@ -209,16 +439,17 @@ F cross(dot a,dot b) {
     return a.x * b.y - a.y * b.x;
 }
 
-dot dsc(dot a,dot b) {
-    return {a.x - b.x,a.y - b.y};
-}
+// dot dsc(dot a,dot b) {
+//     return {a.x - b.x,a.y - b.y};
+// }
 
-dot add(dot a,dot b) {
-    return {a.x + b.x,a.y + b.y};
-}
+// dot add(dot a,dot b) {
+//     return {a.x + b.x,a.y + b.y};
+// }
 
 F cross(dot p1,dot p2,dot p0) {
-    return cross(dsc(p1,p0),dsc(p2,p0));
+    // return cross(dsc(p1,p0),dsc(p2,p0));
+    cross(p1-p0,p2-p0);
 }
 
 int sign(int x) {
@@ -239,12 +470,18 @@ bool onseg(lin l,dot p) {
 
 
 dot linInt(lin a,lin b) {
-    F f = cross( dsc(b.se,b.fi),dsc(a.fi,b.fi) ) / cross(dsc(b.se,b.fi),dsc(a.fi,a.se));
-    auto [tx,ty] = dsc(a.se,a.fi);
-    return add(a.fi,{tx * f,ty * f});
+    // F f = cross( dsc(b.se,b.fi),dsc(a.fi,b.fi) ) / cross(dsc(b.se,b.fi),dsc(a.fi,a.se));
+
+
+    F f = cross( b.se-b.fi,a.fi-b.fi ) / cross(b.se-b.fi,a.fi-a.se);
+    // auto [tx,ty] = dsc(a.se,a.fi);
+    // auto tmp = a.se;
+    auto [tx,ty] = a.se-a.fi;
+
+    return a.fi + dot({tx * f,ty * f});
 }
 
-template<class T> tuple<int, dot, dot> segInt(lin l1, lin l2) {
+tuple<int, dot, dot> segInt(lin l1, lin l2) {
     auto [s1, e1] = l1;
     auto [s2, e2] = l2;
     auto A = max(s1.x, e1.x), AA = min(s1.x, e1.x);
@@ -258,9 +495,9 @@ template<class T> tuple<int, dot, dot> segInt(lin l1, lin l2) {
         if (sign(cross(s2, e1, s1)) != 0) {
             return {0, {}, {}};
         }
-        F p1(max(AA, CC), max(BB, DD));
-        F p2(min(A, C), min(B, D));
-        if (!pointOnSegment(p1, l1)) {
+        dot p1(max(AA, CC), max(BB, DD));
+        dot p2(min(A, C), min(B, D));
+        if (!onseg(l1,p1)) {
             swap(p1.y, p2.y);
         }
         if (p1 == p2) {
@@ -331,9 +568,15 @@ bool inside(lin vec1,lin vec2,dot p) {
 }
 
 lin ext(lin lc) {
-    auto vec = dsc(lc.fi,lc.se);
-    vec = {vec.fi * (1e9),vec.se * (1e9)};
-    return {lc.se,dsc(lc.se,vec)};
+    // auto vec = dsc(lc.fi,lc.se);
+    auto vec =lc.fi-lc.se;
+    // int esp = min()
+    F esp(1e18);
+    if(vec.fi != F(0)) esp = min(F(esp),F(2e10)/abs(vec.fi));
+    if(vec.se != F(0)) esp = min(F(esp),F(2e10)/abs(vec.se));
+    assert(esp != 1e18);
+    vec = dot({vec.fi * esp,vec.se * esp});
+    return {lc.se,lc.se-vec};
 }
 
 bool ifAvil(lin vec1,lin vec2,lin seg) {
@@ -357,14 +600,7 @@ dot convexInt(vector<dot> ch,lin a) {
     vector<dot> ints;
     for(int i = 0;i < ch.size();i ++) {
         auto [status,p1,p2] = segInt(tmp,lin(ch[i],ch[(i+1)%ch.size()] ) );
-        if(status == 1) {
-            return p1;
-        } else if(status == 2) {
-            ints.pb(p1);
-            ints.pb(p2);
-        } else if(status == 3) {
-            return p1;
-        }
+        if(status != )
     }
     // for(auto x:ch) {
     //     auto [status,p,_] = segInt(tmp,x);
@@ -391,6 +627,9 @@ void solve()
 
         // 先判断m0和m1的位置关系
         // 目标：m2t所在直线。可行域：排除：线段m2，包含：线段m1
+
+
+
 
         // auto m2_T = {mirr(m1,m2.fi),mirr(m1,m2.se)};         // 
         // auto m1_TT = {mirr(m0,m1.fi),mirr(m0,m1.se)};
